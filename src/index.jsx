77b38e117476c59bb9728c31c9251a0d86d97d0f
Sb93 <66685463+Shadowblazer93@users.noreply.github.com>
2025-10-24 19:28:33 +0800

    stkoverflow help

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Store from './Store';
+import { BrowserRouter } from 'react-router-dom/BrowserRouter';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
@@ -9,9 +10,9 @@ import * as serviceWorker from 'app/service-worker';
 import App from './App';
 
 ReactDOM.render(
-  <Store>
+  <BrowserRouter basename={process.env.PUBLIC_URL || '/SIERRA'}>
     <App />
-  </Store>,
+  </BrowserRouter>,
   document.getElementById('app'),
 );
 
